d256a6434566db9b468468a7296a75ebf487fcd3
erfan <erfan01724@gmail.com>
2024-02-09 19:27:37 +0330

    Starting ActivityScreen

---
+++ lib/screens/ActivityScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ActivityScreen extends StatefulWidget {
+  const ActivityScreen({super.key});
+
+  @override
+  State<ActivityScreen> createState() => _ActivityScreenState();
+}
+
+class _ActivityScreenState extends State<ActivityScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ErfAn'),
+      ),
+    );
+  }
+}
